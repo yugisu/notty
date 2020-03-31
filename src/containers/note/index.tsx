9de0d87ae524888594967c334c3fe762489c0c90
Dmytrii Puzyr <slymahel@gmail.com>
2020-04-01 01:04:04 +0300

    Move <Note /> related styled components to a separate file

--- src/containers/note/index.tsx
+++ src/containers/note/index.tsx
@@ -10,66 +10,7 @@ import { updateNote, addNote, removeNote } from '~store/notes/actions'
 
 import { Textarea } from '~components/textarea'
 
-const Container = styled.div`
-  position: relative;
-  width: 100%;
-  min-width: 20rem;
-
-  background-color: ${props => props.theme.colors.bg};
-  border: 1px solid #444;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);
-`
-
-const Header = styled.div`
-  position: sticky;
-  top: 0;
-
-  padding: 0.5rem 1rem;
-  display: flex;
-
-  background-color: inherit;
-
-  border-bottom: 1px solid;
-  border-bottom-color: inherit;
-`
-
-const Utilities = styled.div`
-  margin-left: auto;
-
-  display: flex;
-  flex-wrap: nowrap;
-`
-
-const Utility = styled.button`
-  padding: 0.15rem 0.25rem;
-
-  color: #444;
-  font-size: 0.8em;
-
-  opacity: 0.6;
-
-  background: none;
-  border: none;
-  cursor: pointer;
-
-  &:hover,
-  &:focus {
-    opacity: 1;
-  }
-`
-
-const Field = styled(Textarea)`
-  min-height: 10rem;
-  background: inherit;
-
-  &:focus {
-    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
-  }
-`
-
-const PlainInner = styled.div`
-  padding: 1rem;
-`
+import * as S from './styled'
 
 type Props = {
   itemId: string
@@ -114,25 +55,24 @@ export const Note = ({ itemId }: Props) => {
   }
 
   return (
-    <Container>
-      <Header>
+    <S.Container>
+      <S.Header>
         <div></div>
-        <Utilities>
+        <S.Utilities>
           {!isNew && (
-            <Utility onClick={toggleEditing}>{isEditing ? 'Stop editing' : 'Edit'}</Utility>
+            <S.Utility onClick={toggleEditing}>{isEditing ? 'Stop editing' : 'Edit'}</S.Utility>
           )}
-          <Utility onClick={handleDelete}>{isNew ? 'Cancel' : 'Delete'}</Utility>
-        </Utilities>
-      </Header>
+          <S.Utility onClick={handleDelete}>{isNew ? 'Cancel' : 'Delete'}</S.Utility>
+        </S.Utilities>
+      </S.Header>
+
       {isEditing ? (
-        <>
-          <Field value={note?.body ?? ''} onChange={handleBodyUpdate} autoFocus />
-        </>
+        <S.Field value={note?.body ?? ''} onChange={handleBodyUpdate} autoFocus />
       ) : (
-        <PlainInner onDoubleClick={toggleEditing}>
-          <Markdown children={note?.body ?? ''} />
-        </PlainInner>
+        <S.PlainInner onDoubleClick={toggleEditing}>
+          <S.Markdown children={note?.body ?? ''} />
+        </S.PlainInner>
       )}
-    </Container>
+    </S.Container>
   )
 }
