37c6f0e1a3761634f0897b6f283e109293f104cc
Dmytrii Puzyr <slymahel@gmail.com>
2020-04-03 01:29:58 +0300

    Prevent edit mode from triggering on input doubleclick

--- src/components/markdown/components/custom-input.tsx
+++ src/components/markdown/components/custom-input.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
-export const CustomInput = ({ readOnly, ...props }: React.ComponentProps<'input'>) => (
-  <input readOnly={props.type === 'checkbox' ? false : readOnly} {...props} />
+export const CustomInput = ({
+  readOnly,
+  onDoubleClick,
+  ...props
+}: React.ComponentProps<'input'>) => (
+  <input
+    onDoubleClick={e => (e.stopPropagation(), onDoubleClick?.(e))}
+    readOnly={props.type === 'checkbox' ? false : readOnly}
+    {...props}
+  />
 )
