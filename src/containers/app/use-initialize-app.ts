2a62d121075d8f8370821e63dea36cf60269fe2c
Dmytrii Puzyr <slymahel@gmail.com>
2020-06-14 11:55:23 +0300

    Update app to react to localstorage changes

--- src/containers/app/use-initialize-app.ts
+++ src/containers/app/use-initialize-app.ts
@@ -1,7 +1,10 @@
 import { useEffect } from 'react'
-import { entries } from 'lodash/fp'
+import { useDispatch } from 'react-redux'
+import { entries, debounce } from 'lodash/fp'
 
 import { isNoteId } from '~helpers/notes/note-id'
+import { updateAllNotes } from '~store/notes/actions'
+import { getNotes } from '~helpers/notes/get-notes-from-cache'
 
 const cleanUpLocalStorage = () =>
   entries(localStorage)
@@ -12,5 +15,18 @@ const cleanUpLocalStorage = () =>
     .forEach(([key]) => localStorage.removeItem(key))
 
 export const useInitializeApp = () => {
+  const dispatch = useDispatch()
+
   useEffect(() => cleanUpLocalStorage(), [])
+
+  useEffect(() => {
+    const reactToStorageChanges = debounce(500, () => {
+      const notesRecord = getNotes()
+      dispatch(updateAllNotes(notesRecord))
+    })
+
+    window.addEventListener('storage', reactToStorageChanges)
+
+    return () => window.removeEventListener('storage', reactToStorageChanges)
+  }, [dispatch])
 }
