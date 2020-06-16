62835a3e36f47fe53766788477e7c1fbcf956b61
Dmytrii Puzyr <slymahel@gmail.com>
2020-06-16 15:44:39 +0300

    Refactor and improve <Note /> component and the logic which stands behind creation of a new note

--- src/containers/app/index.tsx
+++ src/containers/app/index.tsx
@@ -4,8 +4,11 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 import { useInitializeApp } from './use-initialize-app'
 
 import { GlobalStyles } from '~styles/global'
-import { ViewNote } from '~views/view-note'
 import { NotesCache } from '~components/notes-cache'
+import { Page } from '~containers/page'
+import { NewNote } from '~containers/note/new-note'
+import { Dashboard } from '~views/dashboard'
+import { ViewNote } from '~views/view-note'
 
 export const App = () => {
   useInitializeApp()
@@ -15,11 +18,18 @@ export const App = () => {
       <GlobalStyles />
       <NotesCache />
 
-      <Switch>
-        <Route path="/:noteId([A-z0-9]{6}|new)" component={ViewNote} />
+      <Page>
+        <Page.Header />
+        <Page.Body>
+          <Switch>
+            <Route path="/:noteId([A-z0-9]{6})" component={ViewNote} />
+            <Route path="/new" component={NewNote} />
+            <Route path="/" exact component={Dashboard} />
 
-        <Route render={() => <Redirect to="/new" />} />
-      </Switch>
+            <Route render={() => <Redirect to="/" />} />
+          </Switch>
+        </Page.Body>
+      </Page>
     </>
   )
 }
