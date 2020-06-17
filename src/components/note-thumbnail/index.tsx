66735f53d61eb524ed2ed7187c9dd10ceb24f466
Dmytrii Puzyr <slymahel@gmail.com>
2020-06-17 22:09:07 +0300

    Improve notes list rendering performance and visuals

--- src/components/note-thumbnail/index.tsx
+++ src/components/note-thumbnail/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { omit } from 'lodash/fp'
+import { shallowEqual } from 'react-redux'
 
 import { NoteType } from '~types/note'
 import { extractTitleFromNoteBody } from '~helpers/notes/body-to-title'
@@ -11,42 +12,50 @@ type Props = {
   highlighted?: boolean
 }
 
-export const NoteThumbnail = ({ note, highlighted = false }: Props) => {
-  return (
-    <S.Container>
-      <S.NoteSnapshot
-        role="img"
-        children={note.body}
-        options={{
-          overrides: {
-            a: {
-              component: S.LinkInPreview,
-              props: { as: 'span' },
+export const NoteThumbnail = React.memo(
+  ({ note, highlighted = false }: Props) => {
+    return (
+      <S.Container>
+        <S.NoteSnapshot
+          role="img"
+          aria-hidden
+          children={note.body}
+          options={{
+            overrides: {
+              a: {
+                component: S.LinkInPreview,
+                props: { as: 'span' },
+              },
             },
-          },
-          createElement(type, props, children) {
-            if (typeof type === 'string') {
-              const isFocusable = ['input', 'textarea', 'button', 'a'].includes(type)
+            createElement(type, props, children) {
+              if (typeof type === 'string') {
+                const isFocusable = ['input', 'textarea', 'button', 'a'].includes(type)
 
-              if (isFocusable) {
-                Object.assign(props, { tabIndex: -1 })
+                if (isFocusable) {
+                  Object.assign(props, { tabIndex: -1 })
+                }
               }
-            }
 
-            return React.createElement(type, omit(['id'], props as any), children)
-          },
-        }}
-      />
-      <S.Title highlighted={highlighted}>{extractTitleFromNoteBody(note.body) || note.id}</S.Title>
-    </S.Container>
-  )
-}
+              return React.createElement(type, omit(['id'], props as any), children)
+            },
+          }}
+        />
+        <S.Title highlighted={highlighted}>
+          {extractTitleFromNoteBody(note.body) || note.id}
+        </S.Title>
+      </S.Container>
+    )
+  },
+  (props, newProps) =>
+    shallowEqual(props.note, newProps.note) && props.highlighted === newProps.highlighted,
+)
 
-export const NewNoteThumbnail = () => {
-  return (
+export const NewNoteThumbnail = React.memo(
+  () => (
     <S.Container>
       <S.NewNoteSnapshot>+</S.NewNoteSnapshot>
       <S.Title>Add a new note</S.Title>
     </S.Container>
-  )
-}
+  ),
+  () => true,
+)
