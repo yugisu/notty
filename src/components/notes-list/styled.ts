0bbc35bb1e4a93ef1867926bdb3a36ee7cb39353
Dmytrii Puzyr <slymahel@gmail.com>
2020-06-26 00:25:50 +0300

    Add layout responsiveness to the app

--- src/components/notes-list/styled.ts
+++ src/components/notes-list/styled.ts
@@ -2,45 +2,63 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { transparentize } from 'polished'
 
+import { desktop, tablet } from '~helpers/media'
+
 export const Container = styled.ul`
-  max-height: 100%;
-
   list-style: none;
-  margin: 0;
-  padding: 1rem 1rem 2rem 0;
 
-  overflow-x: hidden;
-  overflow-y: auto;
+  max-height: 100%;
+  margin: 0;
+
+  padding: 1rem;
+  display: flex;
+  overflow-x: scroll;
+
+  ${desktop.css`
+    padding: 1rem 1rem 2rem 0;
+    flex-direction: column;
+
+    overflow-x: hidden;
+    overflow-y: auto;
+  `}
 
   > *:not(:first-child) {
-    margin-top: 1rem;
+    ${tablet.css`
+      margin-left: 0.75rem;
+    `}
+
+    ${desktop.css`
+      margin-top: 1rem;
+    `}
   }
 
-  /* Scrollbar */
+  @media ${desktop.query} {
+    /* Scrollbar */
 
-  scrollbar-width: thin;
-  scrollbar-color: transparent transparent;
+    scrollbar-width: thin;
+    scrollbar-color: transparent transparent;
 
-  &::-webkit-scrollbar {
-    width: 0.35rem;
-  }
+    &::-webkit-scrollbar {
+      width: 0.35rem;
+    }
 
-  &::-webkit-scrollbar-track {
-    background: transparent;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background: transparent;
-  }
-
-  &:hover {
-    scrollbar-color: ${props => transparentize(0.8, props.theme.colors.lightGrey)} transparent;
+    &::-webkit-scrollbar-track {
+      background: transparent;
+    }
 
     &::-webkit-scrollbar-thumb {
-      background: ${props => transparentize(0.8, props.theme.colors.lightGrey)};
+      background: transparent;
+    }
 
-      &:hover {
-        background: ${props => transparentize(0.4, props.theme.colors.lightGrey)};
+    &:hover {
+      scrollbar-color: ${props => transparentize(0.8, props.theme.colors.lightGrey)} transparent;
+
+      &::-webkit-scrollbar-thumb {
+        background: ${props => transparentize(0.8, props.theme.colors.lightGrey)};
+
+        &:hover {
+          background: ${props => transparentize(0.4, props.theme.colors.lightGrey)};
+        }
       }
     }
   }
--- src/containers/page/components/header.tsx
+++ src/containers/page/components/header.tsx
@@ -1,26 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { desktop } from '~helpers/media'
 import { useCurrentTheme } from '~containers/app-provider/components/current-theme-provider'
 
 import { Logo } from '~components/logo'
 import { NotesList } from '~components/notes-list'
 
 const Container = styled.header`
-  position: fixed;
-  top: 0;
-  max-height: 100%;
-  max-width: 20rem;
+  ${desktop.css`
+    position: fixed;
+    top: 0;
+    max-height: 100%;
+    max-width: 20rem;
 
-  padding-left: 2rem;
+    padding-left: 2rem;
+  `}
+
   display: flex;
   flex-direction: column;
 `
 
 const Meta = styled.div`
-  padding: 1.5rem 1rem 1rem 0;
+  padding: 1rem;
   display: flex;
   align-items: center;
+
+  ${desktop.css`
+    padding: 1.5rem 1rem 1rem 0;
+  `}
 `
 
 const ThemeToggler = styled.button`
