cd0664a70e956f93b41f9064544db36c7393e0f2
Dmytrii Puzyr <slymahel@gmail.com>
2020-06-27 13:26:33 +0300

    Implement opening last edited note when visiting `/` page

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
 export const Dashboard = () => {
-  return <div />
+  const history = useHistory()
+  const lastNoteId = useSelector(state => state.notes.keys[0])
+
+  useEffect(() => {
+    history.push(`/${lastNoteId ?? 'new'}`)
+  }, [history, lastNoteId])
+
+  return null
 }
