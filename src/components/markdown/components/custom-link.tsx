55f745b74e1498bec2fdf5b9efe17e0fad938039
Dmytrii Puzyr <slymahel@gmail.com>
2020-08-16 19:26:58 +0300

    Fix TS compiler errors

--- src/components/markdown/components/custom-link.tsx
+++ src/components/markdown/components/custom-link.tsx
@@ -1,7 +1,7 @@
 import React, { HTMLAttributes } from 'react'
 import { Link, LinkProps } from 'react-router-dom'
 
-type Props = LinkProps & HTMLAttributes<HTMLAnchorElement>
+type Props = Omit<LinkProps, 'to'> & HTMLAttributes<HTMLAnchorElement>
 
 export const CustomLink = ({ children, ...props }: Props) => {
   const localHref = props.href?.replace(/https?:\/\//i, '') ?? ''
